64f298f05c6ccb2bd5ad121c012842020eb11267
Cong <buinguyencong@gmail.com>
2017-05-11 19:46:15 +0700

    no message

--- Homeworkss2/HomeworkSS2.turtle.1.py
+++ Homeworkss2/HomeworkSS2.turtle.1.py
@@ -2,7 +2,7 @@ from turtle import *
 color("red")
 speed(-1)
 
-side = 50 # length side of the rhombus
+side = 50 # side length of the rhombus
 x = 20 # smaller angle of the rhombus
 n = 9 # the number of rhombuses
 
